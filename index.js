f2351d769ed942324e244fb8bb3e88f7c78f2d9c
GreenLightning07 <cwnaze@gmail.com>
2023-01-31 10:18:50 -0500

    worked on mobile nav bar

---
+++ index.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('.menu');
+let open = false;
+menu.addEventListener('click', navClicked);
+
+function navClicked() {
+    if(open){
+        document.querySelector('.overlay').style.width='0';
+        menu.style.rotate='0deg';
+        open = false;
+    } else {
+        document.querySelector('.overlay').style.width='100%';
+        menu.style.rotate='90deg';
+        document.querySelector('header').style.zIndex=0;
+        menu.style.zIndex=2;
+        open = true;
+    }
+}
